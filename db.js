44c217d8e5eb5a7144e75d9b52a8708ec593f6af
Michael Liu <michaelliu56@gmail.com>
2018-07-13 01:55:45 -0400

    modify db js

--- db.js
+++ db.js
@@ -1,15 +1,20 @@
 var mysql = require("mysql");
 
+
 function login() {
-    var connection = mysql.createConnection({
+    var connection;
+    if (process.env.JAWSDB_URL) {
+        connection = mysql.createConnection(process.env.JAWSDB_URL);
+    } else {
+        connection = mysql.createConnection({
         host: "localhost",
         port: 3306,
         user: "root", 
         password: "root", 
         database: "hat_store", 
-        multipleStatements: true
+        multipleStatements: true       
     });
-
+    }
     return connection;
 }
 
